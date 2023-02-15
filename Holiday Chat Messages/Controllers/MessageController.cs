9cc1673eb7d7ed27ca3536a1b46dc8259075668e
Kinukeo <jack.noakes@hotmail.com>
2023-02-15 17:13:52 +0000

    feat: created POST request for UserMessage

---
+++ Holiday Chat Messages/Controllers/MessageController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Holiday_Chat_Messages.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        [HttpPost("UserMessage")]
+        public IActionResult UserMessage()
+        {
+            return Ok("User Message is OK");
+        }
+    }
+}
---
+++ Holiday Chat Messages/Models/Requests/UserMessageRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace Holiday_Chat_Messages.Models.Requests
+{
+    public class UserMessageRequest
+    {
+        public string Sender { get; set; }
+        public string Content { get; set; }
+    }
+}
