f86e5c527b878365a283c18ff248b3e83a06a78f
Kinukeo <jack.noakes@hotmail.com>
2023-02-15 15:53:06 +0000

    Add project files.

---
+++ Holiday Chat Messages/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Holiday_Chat_Messages
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
